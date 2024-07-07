23250d59646c0eae2d416b0d546b4081ef07d695
WAHU0508 <sharongikenye@gmail.com>
2024-07-07 16:22:08 +0300

    Mini Task Lister

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const form = document.querySelector('form')
+  form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    
+    addToDoList(e.target.new_task_description.value)
+    form.reset()
+  })
+  
 });
+function addToDoList(todo) {
+  let li = document.createElement('li');
+  let button = document.createElement('button');
+  button.addEventListener('click', deleteTask)
+  li.textContent = `${todo} `
+  button.textContent = 'X'
+  li.appendChild(button)
+  console.log(li)
+  document.querySelector('#tasks').appendChild(li);
+
+}
+function deleteTask(e) {
+  return e.target.parentNode.remove()
+}
